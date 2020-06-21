860f6897160278732bb64f833cbc2b40122fc4a2
Abbas Azisov <abbastktl@gmail.com>
2020-06-21 17:55:11 +0600

    changed default landing page from weathercontest

--- Yourthome/Models/Rental.cs
+++ Yourthome/Models/Rental.cs
@@ -14,7 +14,6 @@ namespace Yourthome.Models
         public int Rooms { get; set; }
         public string Description { get; set; }
         public List<Photo> Photos { get; set; }
-
     }
     public enum Region
     {
